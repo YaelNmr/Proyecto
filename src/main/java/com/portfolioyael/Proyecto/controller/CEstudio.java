569cb337c4ea9f1eb0fe6ad0ed801552242b4dea
YaelNmr <yayithejoker90@hotmail.com>
2023-05-13 23:30:53 -0300

    probando arreglo de cors

--- src/main/java/com/portfolioyael/Proyecto/controller/CEstudio.java
+++ src/main/java/com/portfolioyael/Proyecto/controller/CEstudio.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("estudio")  //localhost:8080/estudio
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-frontend-yael.web.app/")  //localhost:4200
 public class CEstudio {
     
     @Autowired
